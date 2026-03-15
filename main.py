f86a8350c68d68ae26a4792c1fdd5ff1b985f838
Vlad <vboulchouk@gmail.com>
2026-03-15 01:21:06 -0300

    added ai models compatibility. Fixed small errors and typos

--- main.py
+++ main.py
@@ -29,7 +29,9 @@
 
     #--------------------------------------------------------------------------------------------------------#
     #--------------------------------------------------------------------------------------------------------#
-
+#Herramientas prescindibles
+def debug_print(text):
+    print("[DEBUG]*|*|*|*|*|*"+text)
 
 import json                             
 import sounddevice as sd               
@@ -42,16 +44,17 @@ import os
 import sqlite3   
 import threading                       
 import time
-                  
+from llama_cpp import Llama
+
 class Violetta:
     def __init__(self):
         print(os.getcwd())
         with open("config.json", "r", encoding="utf-8") as f:
             parameters = json.load(f)
-            self.Azul=azul(parameters["azul"]["db"],parameters["azul"]["model"])
-            self.Rosa=rosa(parameters["rosa"]["db"],parameters["rosa"]["model"])
-            self.Listener=listener(parameters["listener"]["model"],parameters["listener"]["samplerate"],parameters["listener"]["hear"],parameters["listener"]["wait"])
-            self.Speaker=speaker(parameters["speaker"]["model"],parameters["speaker"]["talk"],parameters["speaker"]["sh"],parameters["speaker"]["repetir"])
+            self.Azul=Azul(parameters["azul"]["db"],parameters["azul"]["model"],parameters["azul"]["ai_enabled"])
+            self.Rosa=Rosa(parameters["rosa"]["db"],parameters["rosa"]["model"],parameters["rosa"]["ai_enabled"])
+            self.Listener=Listener(parameters["listener"]["model"],parameters["listener"]["samplerate"],parameters["listener"]["hear"],parameters["listener"]["wait"])
+            self.Speaker=Speaker(parameters["speaker"]["model"],parameters["speaker"]["talk"],parameters["speaker"]["sh"],parameters["speaker"]["repetir"])
             self.cola_inputs=queue.Queue()
             self.charla=parameters["violetta"]["charla"]
             self.quit_com=parameters["violetta"]["quitc"]
@@ -65,7 +68,7 @@ class Violetta:
         #creando hilos
         self.t_in_read=threading.Thread(target=self.read_in, daemon=True)
         self.t_in_hear=threading.Thread(target=self.hear_in, daemon=True)
-        self.t_consumidor=threading.Thread(target=self.consumidor_inpucts, daemon=True)
+        self.t_consumidor=threading.Thread(target=self.consumidor_inputs, daemon=True)
         #lanzando hilos
         self.t_in_read.start()
         self.t_in_hear.start()
@@ -76,7 +79,7 @@ class Violetta:
         self.t_consumidor.join()
 
 
-    def consumidor_inpucts(self):
+    def consumidor_inputs(self):
         while True:
             chan, entrada = self.cola_inputs.get()
             self.decide(entrada)
@@ -85,45 +88,52 @@ class Violetta:
     
     def read_in(self):
         while True:
-            self.cola_inputs.put(('read', self.filter_wish(input("Escriba lo que desea que haga:" \
+            self.cola_inputs.put(('read', self.filter_wish(input("[VIOLETTA]**Qué quisieras que haga ahora?" \
             ""))))
-            time.sleep(20)
     
     def hear_in(self):
         while True:
             self.cola_inputs.put(('hear', self.filter_wish(self.Listener.listen())))
-            time.sleep(20)
     
     def filter_wish(self,raw_wish):
         raw_wish = raw_wish or ""
-        return re.sub(r'[^a-z0-9 _()""]', '', raw_wish.lower())
+        return re.sub(r'[^a-z0-9 _()"]', '', raw_wish.lower())
 
     def decide(self,elemento):
-        if elemento in self.quit_com:
+        if self.quit_com in elemento:
             exit()
-        elif elemento in self.hear_com:
+        elif self.hear_com in elemento:
             self.Listener.hear=True
-        elif elemento in self.deaf_com:
+        elif self.deaf_com in elemento:
             self.Listener.hear=False
-        elif elemento in self.charlar:
+        elif self.charlar in elemento:
             self.charla=True
-        elif elemento in self.exc:
+        elif self.exc in elemento:
             self.charla=False
         elif self.charla==True:
             self.Rosa.respuesta(elemento)
         else:
             self.Azul.execute_wish(elemento)
+
                 
         
-class azul:
-    def __init__(self, db, model):
+class Azul:
+    def __init__(self, db, model, enabled):
         self.data_base = db
         self.data = sqlite3.connect(self.data_base)
         self.data_cursor = self.data.cursor()
-        print(model)
-        self.model = Model(model)#, from_file=True)
+        self.ai_enabled = enabled
+        if self.ai_enabled:
+            print(model)
+            self.model = Llama(
+            model_path=model,
+            n_ctx=2048,
+            n_threads=8
+            )
 
     def execute_wish(self,wish):
+        if self.ai_enabled:
+            wish = self.model(wish,max_tokens=50)
         if os.path.exists(self.data_base):
             self.data_cursor.execute("SELECT script FROM commands WHERE alias = ?", (wish,))
             row = self.data_cursor.fetchone()
@@ -131,10 +141,12 @@ class azul:
                 execution = "aux_scripts." + row[0]
                 print(execution)
                 exec(execution)
+                hice = "el usuario te pidio que hagas " + wish + ", hiciste," + execution
             else:
-                self.learn_new_command(wish)
+                hice = self.learn_new_command(wish)
         else: 
-            self.learn_new_command(wish)
+            hice = self.learn_new_command(wish)
+        return hice
 
 
     def learn_new_command(self,command):
@@ -144,24 +156,25 @@ class azul:
             script TEXT
         )
         ''')
-        print (f"Your action is {command}. Let´s set it up!\n")
+        print (f"[AZUL]**Me pediste: {command}, no obstante no tengo esa acción incorporada. Ayudame a configurarla! \n")
         #now let´s learn that command or arguments (a bunch of if´s and some other scripts and arguments)
+        hice = "el usuario te pidio que hagas " + command + ", no hiciste nada. no tenes ese comando, el usuario no quiso configurarlo ahora"
 
-        func_name = input("Function name?\nYou can always say exit or cancel to go back\n")
+        func_name = input("[AZUL]**Nombre de la función?\n[AZUL]**Si te arrepentiste, podés echarte atrás con back, quit, salir, y otras expresiones similares.\n")
         if func_name.lower() in ["salir", "exit", "quit", "back", "atras", "volver"]:
-            return
-        new = input("Is it a brand new command?\n")
+            return hice
+        new = input("[AZUL]**Es una función nueva?\n")#-----------------------agregar los chequeos para que no pregunte bobadas cuando coincide con funcion existente
         if new.lower() in ["yes", "si", "y", "s", "sep", "yeah"]:
             self.write_new_function()  
-        argsbase =  input("Function args?\n")
+        argsbase =  input("[AZUL]**Qué argumentos usa?\n[AZUL]**Ejemplo:\n[AZUL]**1,pizza,doom")
         args = [x.strip() for x in argsbase.split(",")]
         for arg in args:
             self.data_cursor.execute("SELECT path FROM path WHERE element = ?", (arg,))
             row = self.data_cursor.fetchone()
             if not row:
-                ispath = input ("This name has a path? Y/N")
+                ispath = input ("[AZUL]**Este argumento es una ruta a un archivo/carpeta?")
                 if ispath.lower() in ["yes", "si", "y", "s", "sep", "yeah"]:
-                    arg_path =  input("what is the path / value of this argument?\nTo write a path, please use only the /, and never comillas")#------------desde aca
+                    arg_path =  input("[AZUL]**Cuál?\n[AZUL]**Ejemplo:\n[AZUL]**c/users/user/desktop")#------------desde aca
                     mapping_arg = (arg,arg_path)
                     self.data_cursor.execute("INSERT OR REPLACE INTO path VALUES (?, ?)", mapping_arg)
                     self.data.commit()
@@ -170,29 +183,40 @@ class azul:
         mappings = (command, script)
         self.data_cursor.execute("INSERT OR REPLACE INTO commands VALUES (?, ?)", mappings)
         self.data.commit()
-        self.execute_wish(command) 
+        hice = "el usuario te pidio que hagas " + command + ", aprendiste el comando e hiciste " + command        
+        return hice 
 
 
     def write_new_function():
         need_user = "yes"
         if need_user == "yes":
-            print("i don´t have that ability yet")
+            print("[AZUL]**No puedo escribir código todavía.\n[AZUL]**Necesito que lo hagas por mí, ok?")
         else:
             return "yes"
 
 
-class rosa:
-    def __init__(self, db, model):
+class Rosa:
+    def __init__(self, db, model, enabled):
         self.data_base = db
         self.data = sqlite3.connect(self.data_base)
         self.data_cursor = self.data.cursor()
-        print(model)
-        self.model = Model(model)#, from_file=True)
+        self.ai_enabled=enabled
+        if self.ai_enabled:
+            print(model)
+            self.model = Llama(
+            model_path=model,
+            n_ctx=2048,
+            n_threads=8
+            )
     def respuesta (self,inpuct):
-        print(f"***debug*** rosa.respuesta({inpuct})")
+        if self.ai_enabled:
+            response = self.model(inpuct,max_tokens=50)
+        else:
+            response = inpuct #calcular de forma manual, probablemente con otra función
+        print(f"[ROSA]**({response})")
 
 
-class listener:
+class Listener:    #------------------------------REHACER COMPLETO BASADO EN AUDIO TO TEXT
     def __init__(self, model, sample, hear, wait):
         self.model = Model(model)
         self.samplerate = sample
@@ -214,7 +238,7 @@ class listener:
         return np.concatenate(rec, axis=0).ravel()
 
 
-class speaker:
+class Speaker:    #------------------------------REHACER COMPLETO BASADO EN TEXT TO SPEECH
     def __init__(self, model,talk,t_callar,t_repetir):
         self.model = Model(model)
         self.queue = queue.Queue()
